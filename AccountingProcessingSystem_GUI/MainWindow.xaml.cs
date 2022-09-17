c93ef561233462ddb74e88dd9f57b690179736c2
Minamin <minamin37345@gmail.com>
2022-09-17 23:22:47 +0900

    UIの配置。データクラスの定義、データ生成の処理を実装。

--- AccountingProcessingSystem_GUI/MainWindow.xaml.cs
+++ AccountingProcessingSystem_GUI/MainWindow.xaml.cs
@@ -16,13 +16,61 @@ using System.Windows.Shapes;
 namespace AccountingProcessingSystem_GUI
 {
     /// <summary>
+    /// 会計データ
+    /// </summary>
+    public partial class ACCOUNTDATA
+    {
+        public DateTime? Date = null;
+        public string Group = string.Empty;
+        public string Title = string.Empty;
+        public int Paid = 0;
+        public int Income = 0;
+
+        public override string ToString()
+        {
+            string res = "";
+            res += "Date:   " + this.Date.ToString() + Environment.NewLine;
+            res += "Group:  " + this.Group + Environment.NewLine;
+            res += "Title:  " + this.Title + Environment.NewLine;
+            res += "Paid:   " + this.Paid.ToString() + Environment.NewLine;
+            res += "Income: " + this.Income.ToString() + Environment.NewLine;
+
+            return res;
+        }
+    }
+    /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<ACCOUNTDATA> accounts = new List<ACCOUNTDATA>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 会計データを追加します。
+        /// </summary>
+        /// <returns>追加に成功したかどうか</returns>
+        public bool AddNewAccount(ref List<ACCOUNTDATA> datas,bool debug=false)
+        {
+            if(DP_date.SelectedDate == null) return false;
+            var current = new ACCOUNTDATA();
+            current.Date = DP_date.SelectedDate;
+            if(CB_Group.SelectedItem == null) current.Group = String.Empty;
+            else current.Group = CB_Group.SelectedItem.ToString();
+            current.Title = TB_Title.Text;
+            current.Paid = int.Parse(TB_Paid.Text);
+            current.Income = int.Parse(TB_Income.Text);
+            datas.Add(current);
+            if(debug) Console.WriteLine(current.ToString());
+            return true;
+        }
+
+        private void Add_Clicked(object sender, RoutedEventArgs e)
+        {
+            AddNewAccount(ref this.accounts);
         }
     }
 }
