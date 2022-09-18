d1c61aec7c6f39e8e4c23a62ca5faa1444ad7451
Minamin <minamin37345@gmail.com>
2022-09-18 17:51:07 +0900

    会計内容の追加、編集、削除の実装

--- AccountingProcessingSystem_GUI/MainWindow.xaml.cs
+++ AccountingProcessingSystem_GUI/MainWindow.xaml.cs
@@ -20,11 +20,11 @@ namespace AccountingProcessingSystem_GUI
     /// <summary>
     /// 会計データ
     /// </summary>
-    public partial class ACCOUNTDATA
+    public partial class ACCOUNTDATA : IEquatable<ACCOUNTDATA>
     {
         public int Id = 0;
         public DateTime? Date = null;
-        public string Group = string.Empty;
+        public GROUP Group = null;
         public string Title = string.Empty;
         public int Paid = 0;
         public int Income = 0;
@@ -34,12 +34,18 @@ namespace AccountingProcessingSystem_GUI
             string res = "";
             res += "Id:     " + this.Id.ToString() + Environment.NewLine;
             res += "Date:   " + this.Date.ToString() + Environment.NewLine;
-            res += "Group:  " + this.Group + Environment.NewLine;
+            if(this.Group == null) res += "Group:  null" + Environment.NewLine;
+            else res += "Group:  " + this.Group.ToString() + Environment.NewLine;
             res += "Title:  " + this.Title + Environment.NewLine;
             res += "Paid:   " + this.Paid.ToString() + Environment.NewLine;
             res += "Income: " + this.Income.ToString() + Environment.NewLine;
 
             return res;
+        }
+
+        public bool Equals(ACCOUNTDATA other)
+        {
+            return this.Id == other.Id;
         }
     }
 
@@ -73,7 +79,7 @@ namespace AccountingProcessingSystem_GUI
     {
         public int ID { get; set; }
         public DateTime? Date { get; set; }
-        public string Group { get; set; }
+        public GROUP Group { get; set; }
         public string Title { get; set; }
         public int Paid { get; set; }
         public int Income { get; set; }
@@ -98,9 +104,15 @@ namespace AccountingProcessingSystem_GUI
     {
         public List<ACCOUNTDATA> accounts = new List<ACCOUNTDATA>();
         public List<GROUP> groups = new List<GROUP>();
+        public bool IsSelected
+        {
+            get { return this.LV_Datas.SelectedItem != null; }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            this.B_Edit.IsEnabled = this.IsSelected;
+            this.B_Del.IsEnabled = this.IsSelected;
         }
 
         /// <summary>
@@ -113,8 +125,8 @@ namespace AccountingProcessingSystem_GUI
             var current = new ACCOUNTDATA();
             current.Id = datas.Count;
             current.Date = DP_date.SelectedDate;
-            if(CB_Group.SelectedItem == null) current.Group = String.Empty;
-            else current.Group = CB_Group.SelectedItem.ToString();
+            if(CB_Group.SelectedItem == null) current.Group = null;
+            else current.Group = CB_Group.SelectedItem as GROUP;
             current.Title = TB_Title.Text;
             current.Paid = int.Parse(TB_Paid.Text);
             current.Income = int.Parse(TB_Income.Text);
@@ -179,26 +191,41 @@ namespace AccountingProcessingSystem_GUI
         private void Edit_clicked(object sender, RoutedEventArgs e)
         {
             if (this.LV_Datas.SelectedItem == null) return;
-            var target = (this.LV_Datas.SelectedItem as ACCOUNTDATASHOWS).Owner;
+            var target = (this.LV_Datas.SelectedItem as ACCOUNTDATA);
             if (target == null) return;
             target.Date = this.DP_date.SelectedDate;
-            target.Group = this.CB_Group.SelectedItem.ToString();
+            target.Group = this.CB_Group.SelectedItem as GROUP;
             target.Title = this.TB_Title.Text;
             target.Paid = int.Parse(this.TB_Paid.Text);
             target.Income = int.Parse(this.TB_Income.Text);
-        }
-
-        private void EditElementSelected(object sender, MouseButtonEventArgs e)
-        {
-            if (this.LV_Datas.SelectedItem == null) return;
-            var target = (this.LV_Datas.SelectedItem as ACCOUNTDATASHOWS).Owner;
-            this.DP_date.SelectedDate = target.Date;
-            //this.
+            this.ShowDatas(ref this.accounts);
+            this.B_Edit.IsEnabled = this.IsSelected;
         }
 
         private void GroupManager_Clicked(object sender, RoutedEventArgs e)
         {
             this.OpenGroupManager(ref this.groups);
+        }
+
+        private void Del_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (this.LV_Datas.SelectedItem as ACCOUNTDATASHOWS == null) return;
+            this.accounts.Remove((this.LV_Datas.SelectedItem as ACCOUNTDATASHOWS).Owner);
+            this.ShowDatas(ref this.accounts);
+            this.B_Del.IsEnabled = this.IsSelected;
+        }
+
+        private void ElementSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.LV_Datas.SelectedItem as ACCOUNTDATASHOWS == null) return;
+            var target = (this.LV_Datas.SelectedItem as ACCOUNTDATASHOWS).Owner;
+            this.DP_date.SelectedDate = target.Date;
+            this.CB_Group.SelectedItem = target.Group;
+            this.TB_Title.Text = target.Title;
+            this.TB_Paid.Text = target.Paid.ToString();
+            this.TB_Income.Text = target.Income.ToString();
+            this.B_Edit.IsEnabled = this.IsSelected;
+            this.B_Del.IsEnabled = this.IsSelected;
         }
     }
 }
